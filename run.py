4056717e182874649a92858e0887bfc39c3aadb3
matiasinfo <guitarramamadera@gmail.com>
2021-03-24 19:58:50 -0300

    solo un comentario

--- run.py
+++ run.py
@@ -2,13 +2,7 @@
 
 cant = 0
 
-for i in range(4):
-    palabra1 = input("ingrese una palabra")
-
-    if "r" in palabra1:
-        print(f"{palabra1}tiene r")
-    else: 
-        print(f"{palabra1} no tiene r ")
+# comentariios
 
         
 
