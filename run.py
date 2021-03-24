0fc6688c7f7c8d6732195ce1887357896f78c61f
matiasinfo <guitarramamadera@gmail.com>
2021-03-24 19:45:55 -0300

    modifique el programa

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-a = 10
-b = 20
-suma = a + b + a
-print(f"la suma es  {suma}")
+
+
+cant = 0
+
+for i in range(4):
+    palabra1 = input("ingrese una palabra")
+
+    if "r" in palabra1:
+        print(f"{palabra1}tiene r")
+    else: 
+        print(f"{palabra1} no tiene r ")
+
+        
+
+print(" la cantidad de letras a en la palabra es : ",cant)
